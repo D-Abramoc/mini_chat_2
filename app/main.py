76e328f31f04cad9180762316e226b9b608c1506
Dmitry Abramov <elektrik.kem@yandex.ru>
2024-10-30 10:49:06 +0700

    save message

--- app/main.py
+++ app/main.py
@@ -1,15 +1,20 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.endpoints.auth import router as auth_router
 from app.api.endpoints.messages import router as messages_router
 from app.api.endpoints.users import router as users_router
 from app.api.utils.messages import manager
 from app.redis_app import redis as r
+from app.crud.messages import message_crud
+from app.dependencies import get_async_session
 
 app = FastAPI()
 app.mount('/static', StaticFiles(directory='app/static'), name='static')
@@ -33,78 +38,26 @@ async def startup_event():
     FastAPICache.init(RedisBackend(redis=r), prefix='fastapi-cache')
 
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <h2>Your ID: <span id="ws-id"></span></h2>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        
-        <script>
-            var client_id = Date.now()
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://127.0.0.1/ws/${client_id}`);
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
-
-# class ConnectionManager:
-#     def __init__(self):
-#         self.active_connections: list[WebSocket] = []
-
-#     async def connect(self, websocket: WebSocket):
-#         await websocket.accept()
-#         self.active_connections.append(websocket)
-
-#     def disconnect(self, websocket: WebSocket):
-#         self.active_connections.remove(websocket)
-
-#     async def send_personal_message(self, message: str, websocket: WebSocket):
-#         await websocket.send_json(message)
-
-#     async def broadcast(self, message: str):
-#         for connection in self.active_connections:
-#             await connection.send_json(message)
-
-
-# manager = ConnectionManager()
-
-
-# @app.get("/")
-# async def get():
-#     return HTMLResponse(html)
-
-
 @app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
+async def websocket_endpoint(
+    websocket: WebSocket, client_id: int,
+    session: Annotated[AsyncSession, Depends(get_async_session)]
+):
     await manager.connect(client_id, websocket)
     try:
         while True:
             data = await websocket.receive_text()
+            try:
+                to_recipient, message = data.split(maxsplit=1)
+            except ValueError:
+                to_recipient = data.strip()
+                message = 'Отправлено пустое сообщение.'
+            await message_crud.add(
+                session=session,
+                sender_id=client_id,
+                recipient_id=int(to_recipient),
+                content=message
+            )
             await manager.send_personal_message(f"You wrote: {data}", websocket)
             await manager.broadcast(f"Client #{client_id} says: {data}")
     except WebSocketDisconnect:
